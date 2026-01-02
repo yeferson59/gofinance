ad7cfc84b5730f9ad26334c4f6e6c6b35472372f
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-01-01 20:59:09 -0500

    feat: add management for rate interest

---
+++ finance/compositeinterest/rate_conversion.go
@@ -0,0 +1,130 @@
+package compositeinterest
+
+import (
+	"errors"
+	"math"
+)
+
+func (rt *RateInterest) getCompoundingFrequency(compoundingFrequency CompoundingFrequency) (float64, error) {
+	value, ok := countCompoundingFrequency[compoundingFrequency]
+	if !ok {
+		return 0, errors.New("Invalid value compounding frequency")
+	}
+
+	return value, nil
+}
+
+func (rt *RateInterest) RatePeriodic() (float64, error) {
+	if rt.typeRate == RateEffectyPeriodic {
+		return rt.value, nil
+	}
+
+	value, err := rt.getCompoundingFrequency(rt.compoundingFrequency)
+	if err != nil {
+		return 0, err
+	}
+
+	var ratePeriodic float64
+
+	if rt.typeRate == RateEffectyNominal {
+		ratePeriodic = rt.value / value
+	}
+
+	if rt.typeRate == RateEffectyAnnualy {
+		rateNominal := value * (math.Pow((1+rt.value), (1/value)) - 1)
+		ratePeriodic = rateNominal / value
+	}
+
+	return ratePeriodic, nil
+}
+
+func (rt *RateInterest) RateNominal() (float64, error) {
+	if rt.typeRate == RateEffectyNominal {
+		return rt.value, nil
+	}
+
+	value, err := rt.getCompoundingFrequency(rt.compoundingFrequency)
+	if err != nil {
+		return 0, err
+	}
+
+	var rateNominal float64
+
+	if rt.typeRate == RateEffectyAnnualy {
+		rateNominal = value * (math.Pow((1+rt.value), (1/value)) - 1)
+	}
+
+	if rt.typeRate == RateEffectyPeriodic {
+		rateNominal = rt.value * value
+	}
+
+	return rateNominal, nil
+}
+
+func (rt *RateInterest) RateEffectyAnnualy() (float64, error) {
+	if rt.typeRate == RateEffectyAnnualy {
+		return rt.value, nil
+	}
+
+	value, err := rt.getCompoundingFrequency(rt.compoundingFrequency)
+	if err != nil {
+		return 0, err
+	}
+
+	var rateEffectyAnnualy float64
+
+	if rt.typeRate == RateEffectyNominal {
+		ratePeriodic := rt.value / value
+		rateEffectyAnnualy = math.Pow(1+ratePeriodic, value) - 1
+	}
+
+	if rt.typeRate == RateEffectyPeriodic {
+		rateEffectyAnnualy = math.Pow(1+rt.value, value) - 1
+	}
+
+	return rateEffectyAnnualy, nil
+}
+
+func (rt *RateInterest) RatePeriodicToPeriodic(newCompoundingFrequency CompoundingFrequency) (float64, error) {
+	currentRatePeriodic, err := rt.RatePeriodic()
+	if err != nil {
+		return 0, err
+	}
+
+	newValue, err := rt.getCompoundingFrequency(newCompoundingFrequency)
+	if err != nil {
+		return 0, err
+	}
+
+	value, err := rt.getCompoundingFrequency(rt.compoundingFrequency)
+	if err != nil {
+		return 0, err
+	}
+
+	exp := (1 / newValue) * value
+
+	newRatePeriodic := math.Pow(1+currentRatePeriodic, exp) - 1
+
+	return newRatePeriodic, nil
+}
+
+func (rt *RateInterest) RateNominalToNominal(newCompoundingFrequency CompoundingFrequency) (float64, error) {
+	newPeriodic, err := rt.RatePeriodicToPeriodic(newCompoundingFrequency)
+	if err != nil {
+		return 0, err
+	}
+
+	oldCompoundingFrequency, oldValue, oldTypeRate := rt.compoundingFrequency, rt.value, rt.typeRate
+
+	rt.compoundingFrequency = newCompoundingFrequency
+	rt.typeRate = RateEffectyPeriodic
+	rt.value = newPeriodic
+
+	newNominal, err := rt.RateNominal()
+
+	rt.compoundingFrequency = oldCompoundingFrequency
+	rt.typeRate = oldTypeRate
+	rt.value = oldValue
+
+	return newNominal, err
+}
--- finance/compositeinterest/root.go
+++ finance/compositeinterest/root.go
@@ -1,11 +1,105 @@
 package compositeinterest
 
+type CompoundingFrequency string
+type TypeRate string
+
+const (
+	Daily        CompoundingFrequency = "daily"
+	Monthly      CompoundingFrequency = "monthly"
+	Bimonthly    CompoundingFrequency = "Bimontly"
+	QuarterlyOne CompoundingFrequency = "quarterlyOne"
+	QuarterlyTwo CompoundingFrequency = "quarterlyTwo"
+	SemiAnnually CompoundingFrequency = "semiAnnually"
+	Annually     CompoundingFrequency = "annualy"
+)
+
+const (
+	RateEffectyPeriodic TypeRate = "periodic"
+	RateEffectyAnnualy  TypeRate = "annual"
+	RateEffectyNominal  TypeRate = "nominal"
+)
+
+var countCompoundingFrequency = map[CompoundingFrequency]float64{
+	Daily:        365,
+	Monthly:      12,
+	Bimonthly:    6,
+	QuarterlyOne: 4,
+	QuarterlyTwo: 3,
+	SemiAnnually: 2,
+	Annually:     1,
+}
+
 type Period struct {
-	months float64
+	daily        *float64
+	monthly      *float64
+	bimonthly    *float64
+	quarterlyOne *float64
+	quarterlyTwo *float64
+	semiAnnually *float64
+	annually     *float64
+}
+
+func NewPeriod(numberPeriods float64, compoundingFrequency CompoundingFrequency) (*Period, error) {
+	switch compoundingFrequency {
+	case Daily:
+		return &Period{
+			daily: &numberPeriods,
+		}, nil
+	case Monthly:
+		return &Period{
+			monthly: &numberPeriods,
+		}, nil
+	case Bimonthly:
+		return &Period{
+			bimonthly: &numberPeriods,
+		}, nil
+	case QuarterlyOne:
+		return &Period{
+			quarterlyOne: &numberPeriods,
+		}, nil
+	case QuarterlyTwo:
+		return &Period{
+			quarterlyTwo: &numberPeriods,
+		}, nil
+	case SemiAnnually:
+		return &Period{
+			semiAnnually: &numberPeriods,
+		}, nil
+	case Annually:
+		return &Period{
+			annually: &numberPeriods,
+		}, nil
+	default:
+		return &Period{}, nil
+	}
+}
+
+type RateInterest struct {
+	value                float64
+	compoundingFrequency CompoundingFrequency
+	typeRate             TypeRate
+}
+
+func NewRateInterest(value float64, compoundingFrequency CompoundingFrequency, typeRate TypeRate) (*RateInterest, error) {
+	return &RateInterest{
+		value:                value,
+		compoundingFrequency: compoundingFrequency,
+		typeRate:             typeRate,
+	}, nil
 }
 
 type CompositeInterest struct {
 	future       float64
 	present      float64
-	rateInterest float64
+	rateInterest *RateInterest
+	periods      *Period
+}
+
+func New(present, future float64, rateInterest *RateInterest, periods *Period) (*CompositeInterest, error) {
+	return &CompositeInterest{
+		present:      present,
+		future:       future,
+		rateInterest: rateInterest,
+		periods:      periods,
+	}, nil
 }
