f95e8d1996f8db95e5bd3ef1cd497e78cee385ef
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-01-03 14:42:06 -0500

    feat: add simple test

---
+++ finance/simpleinterest/future_test.go
@@ -0,0 +1,92 @@
+package simpleinterest
+
+import "testing"
+
+func TestSuccessSimpleFuture(t *testing.T) {
+	periods := NewPeriod(2, Days)
+	simpleInterest := New(0, 5_000, 1_000, 0, periods)
+	expectedFuture := 6_000.0
+
+	future, err := simpleInterest.Future()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if future == 0 {
+		t.Error(future)
+	}
+
+	if future != expectedFuture {
+		t.Error(future)
+	}
+}
+
+func TestSuccessSimpleFutureWithRateInterest(t *testing.T) {
+	periods := NewPeriod(2, Days)
+	simpleInterest := New(0, 5_000, 0, 0.05, periods)
+	expectedFuture := 5_500.0
+
+	future, err := simpleInterest.FutureWithRateInterest()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if future == 0 {
+		t.Error(future)
+	}
+
+	if future != expectedFuture {
+		t.Error(future)
+	}
+}
+
+func TestSuccessComplexFutureWithRateInterest(t *testing.T) {
+	type DataTest struct {
+		simpleInterest *InterestSimple
+	}
+
+	periods := NewPeriod(2, Days)
+
+	testData := []DataTest{
+		{
+			simpleInterest: New(0, 5_000, 0, 0.02, periods),
+		},
+		{
+			simpleInterest: New(0, 5_000, 0, 0.01, periods),
+		},
+		{
+			simpleInterest: New(0, 5_000, 0, 0.006, periods),
+		},
+		{
+			simpleInterest: New(0, 5_000, 0, 0.0001, periods),
+		},
+		{
+			simpleInterest: New(0, 5_000, 0, 0.05, periods),
+		},
+	}
+
+	t.Run("not error", func(t *testing.T) {
+		for _, data := range testData {
+			value, err := data.simpleInterest.FutureWithRateInterest()
+
+			if value > 6_000.0 {
+				t.Error(value)
+			}
+
+			if err != nil {
+				t.Error(err)
+			}
+		}
+	})
+
+	t.Run("not error", func(t *testing.T) {
+		for _, data := range testData {
+			data.simpleInterest.periods = &Period{}
+			_, err := data.simpleInterest.FutureWithRateInterest()
+
+			if err == nil {
+				t.Error(err)
+			}
+		}
+	})
+}
--- finance/simpleinterest/root_test.go
+++ finance/simpleinterest/root_test.go
@@ -41,6 +41,98 @@ func TestNewPeriod(t *testing.T) {
 	_ = NewPeriodTest(t)
 }
 
+func TestNewPeriodWithDifferentsTimes(t *testing.T) {
+	type DataTest struct {
+		number  float64
+		Periods Periods
+	}
+
+	test := []DataTest{
+		{
+			number:  2,
+			Periods: Days,
+		},
+		{
+			number:  3,
+			Periods: Weeks,
+		},
+		{
+			number:  3,
+			Periods: Months,
+		},
+		{
+			number:  4,
+			Periods: Years,
+		},
+		{
+			number:  4,
+			Periods: "",
+		},
+	}
+
+	t.Run("valid new correct period", func(t *testing.T) {
+		for _, value := range test {
+			period := NewPeriod(value.number, value.Periods)
+
+			if value.Periods == Days {
+				if period.days == nil {
+					t.Error(period.days)
+				}
+			}
+
+			if value.Periods == Weeks {
+				if period.weeks == nil {
+					t.Error(period.weeks)
+				}
+			}
+
+			if value.Periods == Months {
+				if period.months == nil {
+					t.Error(period.months)
+				}
+			}
+
+			if value.Periods == Years {
+				if period.years == nil {
+					t.Error(period.years)
+				}
+			}
+
+			if value.Periods == "" {
+				if period.days != nil && period.weeks != nil && period.months != nil && period.years != nil {
+					t.Error(period.years)
+				}
+			}
+		}
+	})
+
+	t.Run("get valid value for period", func(t *testing.T) {
+		for _, value := range test {
+			period := NewPeriod(value.number, value.Periods)
+
+			valuePeriod, err := period.getPeriod()
+
+			if value.Periods == "" {
+				if valuePeriod != nil {
+					t.Error(valuePeriod)
+				}
+				if err == nil {
+					t.Error(err)
+				}
+			} else {
+				periods := *valuePeriod
+				if periods != value.number {
+					t.Error(valuePeriod)
+				}
+
+				if err != nil {
+					t.Error(err)
+				}
+			}
+		}
+	})
+}
+
 func TestGetPeriod(t *testing.T) {
 	period := NewPeriodTest(t)
 	expectedPeriod := 2.0
@@ -52,5 +144,47 @@ func TestGetPeriod(t *testing.T) {
 
 	if expectedPeriod != *numberPeriod {
 		t.Error("number period and expected period must be equals")
+	}
+}
+
+func TestNewSimpleInterest(t *testing.T) {
+	period := NewPeriodTest(t)
+
+	simpleInterest := New(0, 0, 0, 0, period)
+
+	if simpleInterest.future != 0 {
+		t.Error(simpleInterest.future)
+	}
+
+	if simpleInterest.present != 0 {
+		t.Error(simpleInterest.present)
+	}
+
+	if simpleInterest.interest != 0 {
+		t.Error(simpleInterest.interest)
+	}
+
+	if simpleInterest.rateInterest != 0 {
+		t.Error(simpleInterest.rateInterest)
+	}
+
+	if simpleInterest.periods == nil {
+		t.Error(simpleInterest.periods)
+	}
+}
+
+func TestGetPeriodWithSimpleInterest(t *testing.T) {
+	period := NewPeriodTest(t)
+
+	simpleInterest := New(0, 0, 0, 0, period)
+
+	valuePeriod, err := simpleInterest.GetPeriods()
+
+	if valuePeriod != 2 {
+		t.Error(valuePeriod)
+	}
+
+	if err != nil {
+		t.Error(err)
 	}
 }
