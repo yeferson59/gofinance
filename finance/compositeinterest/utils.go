3fcc72b9782d359ca09ba90c6c5a2d2ee44ef54e
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-01-03 09:42:31 -0500

    fix: change to lower case error message

--- finance/compositeinterest/utils.go
+++ finance/compositeinterest/utils.go
@@ -5,7 +5,7 @@ import "errors"
 func getCompoundingFrequency(compoundingFrequency CompoundingFrequency) (float64, error) {
 	value, ok := countCompoundingFrequency[compoundingFrequency]
 	if !ok {
-		return 0, errors.New("Invalid value compounding frequency")
+		return 0, errors.New("invalid value compounding frequency")
 	}
 
 	return value, nil
