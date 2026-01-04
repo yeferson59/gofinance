877c83dfa4a1302204016b5f57d76655b675d23b
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-01-04 12:54:34 -0500

    feat: add Currency enum and ISO code lookup

---
+++ money/currency.go
@@ -0,0 +1,336 @@
+package money
+
+import "errors"
+
+type Currency uint
+
+const (
+	XXX Currency = 0   // No Currency
+	XTS Currency = 1   // Test Currency
+	AED Currency = 2   // U.A.E. Dirham
+	AFN Currency = 3   // Afghani
+	ALL Currency = 4   // Lek
+	AMD Currency = 5   // Armenian Dram
+	ANG Currency = 6   // Netherlands Antillian Guilder
+	AOA Currency = 7   // Kwanza
+	ARS Currency = 8   // Argentine Peso
+	AUD Currency = 9   // Australian Dollar
+	AWG Currency = 10  // Aruban Guilder
+	AZN Currency = 11  // Azerbaijan Manat
+	BAM Currency = 12  // Convertible Mark
+	BBD Currency = 13  // Barbados Dollar
+	BDT Currency = 14  // Taka
+	BGN Currency = 15  // Bulgarian Lev
+	BHD Currency = 16  // Bahraini Dinar
+	BIF Currency = 17  // Burundi Franc
+	BMD Currency = 18  // Bermudian Dollar
+	BND Currency = 19  // Brunei Dollar
+	BOB Currency = 20  // Boliviano
+	BRL Currency = 21  // Brazilian Real
+	BSD Currency = 22  // Bahamian Dollar
+	BTN Currency = 23  // Bhutan Ngultrum
+	BWP Currency = 24  // Pula
+	BYN Currency = 25  // Belarussian Ruble
+	BZD Currency = 26  // Belize Dollar
+	CAD Currency = 27  // Canadian Dollar
+	CDF Currency = 28  // Franc Congolais
+	CHF Currency = 29  // Swiss Franc
+	CLP Currency = 30  // Chilean Peso
+	CNY Currency = 31  // Yuan Renminbi
+	COP Currency = 32  // Colombian Peso
+	CRC Currency = 33  // Costa Rican Colon
+	CUP Currency = 34  // Cuban Peso
+	CVE Currency = 35  // Cape Verde Escudo
+	CZK Currency = 36  // Czech Koruna
+	DJF Currency = 37  // Djibouti Franc
+	DKK Currency = 38  // Danish Krone
+	DOP Currency = 39  // Dominican Peso
+	DZD Currency = 40  // Algerian Dinar
+	EGP Currency = 41  // Egyptian Pound
+	ERN Currency = 42  // Eritean Nakfa
+	ETB Currency = 43  // Ethiopian Birr
+	EUR Currency = 44  // Euro
+	FJD Currency = 45  // Fiji Dollar
+	FKP Currency = 46  // Falkland Islands Pound
+	GBP Currency = 47  // Pound Sterling
+	GEL Currency = 48  // Lari
+	GHS Currency = 49  // Cedi
+	GIP Currency = 50  // Gibraltar Pound
+	GMD Currency = 51  // Dalasi
+	GNF Currency = 52  // Guinea Franc
+	GTQ Currency = 53  // Quetzal
+	GWP Currency = 54  // Guinea-Bissau Peso
+	GYD Currency = 55  // Guyana Dollar
+	HKD Currency = 56  // Hong Kong Dollar
+	HNL Currency = 57  // Lempira
+	HRK Currency = 58  // Croatian Kuna
+	HTG Currency = 59  // Gourde
+	HUF Currency = 60  // Forint
+	IDR Currency = 61  // Rupiah
+	ILS Currency = 62  // Israeli Shequel
+	INR Currency = 63  // Indian Rupee
+	IQD Currency = 64  // Iraqi Dinar
+	IRR Currency = 65  // Iranian Rial
+	ISK Currency = 66  // Iceland Krona
+	JMD Currency = 67  // Jamaican Dollar
+	JOD Currency = 68  // Jordanian Dinar
+	JPY Currency = 69  // Yen
+	KES Currency = 70  // Kenyan Shilling
+	KGS Currency = 71  // Som
+	KHR Currency = 72  // Riel
+	KMF Currency = 73  // Comoro Franc
+	KPW Currency = 74  // North Korean Won
+	KRW Currency = 75  // Won
+	KWD Currency = 76  // Kuwaiti Dinar
+	KYD Currency = 77  // Cayman Islands Dollar
+	KZT Currency = 78  // Tenge
+	LAK Currency = 79  // Kip
+	LBP Currency = 80  // Lebanese Pound
+	LKR Currency = 81  // Sri Lanka Rupee
+	LRD Currency = 82  // Liberian Dollar
+	LSL Currency = 83  // Lesotho Loti
+	LYD Currency = 84  // Libyan Dinar
+	MAD Currency = 85  // Moroccan Dirham
+	MDL Currency = 86  // Moldovan Leu
+	MGA Currency = 87  // Malagasy Ariary
+	MKD Currency = 88  // Denar
+	MMK Currency = 89  // Kyat
+	MNT Currency = 90  // Tugrik
+	MOP Currency = 91  // Pataca
+	MRU Currency = 92  // Ouguiya
+	MUR Currency = 93  // Mauritius Rupee
+	MVR Currency = 94  // Rufiyaa
+	MWK Currency = 95  // Malawi Kwacha
+	MXN Currency = 96  // Mexican Peso
+	MYR Currency = 97  // Malaysian Ringgit
+	MZN Currency = 98  // Mozambique Metical
+	NAD Currency = 99  // Namibia Dollar
+	NGN Currency = 100 // Naira
+	NIO Currency = 101 // Cordoba Oro
+	NOK Currency = 102 // Norwegian Krone
+	NPR Currency = 103 // Nepalese Rupee
+	NZD Currency = 104 // New Zealand Dollar
+	OMR Currency = 105 // Rial Omani
+	PAB Currency = 106 // Balboa
+	PEN Currency = 107 // Sol
+	PGK Currency = 108 // Kina
+	PHP Currency = 109 // Philippine Peso
+	PKR Currency = 110 // Pakistan Rupee
+	PLN Currency = 111 // Zloty
+	PYG Currency = 112 // Guarani
+	QAR Currency = 113 // Qatari Rial
+	RON Currency = 114 // Leu
+	RSD Currency = 115 // Serbian Dinar
+	RUB Currency = 116 // Russian Ruble
+	RWF Currency = 117 // Rwanda Franc
+	SAR Currency = 118 // Saudi Riyal
+	SBD Currency = 119 // Solomon Islands Dollar
+	SCR Currency = 120 // Seychelles Rupee
+	SDG Currency = 121 // Sudanese Pound
+	SEK Currency = 122 // Swedish Krona
+	SGD Currency = 123 // Singapore Dollar
+	SHP Currency = 124 // St. Helena Pound
+	SLL Currency = 125 // Leone
+	SOS Currency = 126 // Somali Shilling
+	SRD Currency = 127 // Surinam Dollar
+	SSP Currency = 128 // South Sudanese Pound
+	STN Currency = 129 // Dobra
+	SYP Currency = 130 // Syrian Pound
+	SZL Currency = 131 // Lilangeni
+	THB Currency = 132 // Baht
+	TJS Currency = 133 // Somoni
+	TMT Currency = 134 // Manat
+	TND Currency = 135 // Tunisian Dinar
+	TOP Currency = 136 // Pa'anga
+	TRY Currency = 137 // Turkish Lira
+	TTD Currency = 138 // Trinidad and Tobago Dollar
+	TWD Currency = 139 // New Taiwan Dollar
+	TZS Currency = 140 // Tanzanian Shilling
+	UAH Currency = 141 // Ukrainian Hryvnia
+	UGX Currency = 142 // Uganda Shilling
+	USD Currency = 143 // U.S. Dollar
+	UYU Currency = 144 // Peso Uruguayo
+	UZS Currency = 145 // Uzbekistan Sum
+	VES Currency = 146 // Sovereign Bolivar
+	VND Currency = 147 // Dong
+	VUV Currency = 148 // Vatu
+	WST Currency = 149 // Tala
+	XAF Currency = 150 // CFA Franc BEAC
+	XCD Currency = 151 // East Caribbean Dollar
+	XOF Currency = 152 // CFA Franc BCEAO
+	XPF Currency = 153 // CFP Franc
+	YER Currency = 154 // Yemeni Rial
+	ZAR Currency = 155 // Rand
+	ZMW Currency = 156 // Zambian Kwacha
+	ZWL Currency = 157 // Zimbabwe Dollar
+)
+
+var currencyCode = map[Currency]string{
+	XXX: "XXX",
+	XTS: "XTS",
+	AED: "AED",
+	AFN: "AFN",
+	ALL: "ALL",
+	AMD: "AMD",
+	ANG: "ANG",
+	AOA: "AOA",
+	ARS: "ARS",
+	AUD: "AUD",
+	AWG: "AWG",
+	AZN: "AZN",
+	BAM: "BAM",
+	BBD: "BBD",
+	BDT: "BDT",
+	BGN: "BGN",
+	BHD: "BHD",
+	BIF: "BIF",
+	BMD: "BMD",
+	BND: "BND",
+	BOB: "BOB",
+	BRL: "BRL",
+	BSD: "BSD",
+	BTN: "BTN",
+	BWP: "BWP",
+	BYN: "BYN",
+	BZD: "BZD",
+	CAD: "CAD",
+	CDF: "CDF",
+	CHF: "CHF",
+	CLP: "CLP",
+	CNY: "CNY",
+	COP: "COP",
+	CRC: "CRC",
+	CUP: "CUP",
+	CVE: "CVE",
+	CZK: "CZK",
+	DJF: "DJF",
+	DKK: "DKK",
+	DOP: "DOP",
+	DZD: "DZD",
+	EGP: "EGP",
+	ERN: "ERN",
+	ETB: "ETB",
+	EUR: "EUR",
+	FJD: "FJD",
+	FKP: "FKP",
+	GBP: "GBP",
+	GEL: "GEL",
+	GHS: "GHS",
+	GIP: "GIP",
+	GMD: "GMD",
+	GNF: "GNF",
+	GTQ: "GTQ",
+	GWP: "GWP",
+	GYD: "GYD",
+	HKD: "HKD",
+	HNL: "HNL",
+	HRK: "HRK",
+	HTG: "HTG",
+	HUF: "HUF",
+	IDR: "IDR",
+	ILS: "ILS",
+	INR: "INR",
+	IQD: "IQD",
+	IRR: "IRR",
+	ISK: "ISK",
+	JMD: "JMD",
+	JOD: "JOD",
+	JPY: "JPY",
+	KES: "KES",
+	KGS: "KGS",
+	KHR: "KHR",
+	KMF: "KMF",
+	KPW: "KPW",
+	KRW: "KRW",
+	KWD: "KWD",
+	KYD: "KYD",
+	KZT: "KZT",
+	LAK: "LAK",
+	LBP: "LBP",
+	LKR: "LKR",
+	LRD: "LRD",
+	LSL: "LSL",
+	LYD: "LYD",
+	MAD: "MAD",
+	MDL: "MDL",
+	MGA: "MGA",
+	MKD: "MKD",
+	MMK: "MNK",
+	MNT: "MINT",
+	MOP: "MOP",
+	MRU: "MRU",
+	MUR: "MUR",
+	MVR: "MVR",
+	MWK: "MWK",
+	MXN: "MXN",
+	MYR: "MYR",
+	MZN: "MZN",
+	NAD: "NAD",
+	NGN: "NGN",
+	NIO: "NIO",
+	NOK: "NOK",
+	NPR: "NPR",
+	NZD: "NZD",
+	OMR: "OMR",
+	PAB: "PAB",
+	PEN: "PEN",
+	PGK: "PGK",
+	PHP: "PHP",
+	PKR: "PKR",
+	PLN: "PLN",
+	PYG: "PYG",
+	QAR: "QAR",
+	RON: "RON",
+	RSD: "RSD",
+	RUB: "RUB",
+	RWF: "RWF",
+	SAR: "SAR",
+	SBD: "SBD",
+	SCR: "SCR",
+	SDG: "SDG",
+	SEK: "SEK",
+	SGD: "SGD",
+	SHP: "SHP",
+	SLL: "SLL",
+	SOS: "SOS",
+	SRD: "SRD",
+	SSP: "SSP",
+	STN: "STN",
+	SYP: "SYP",
+	SZL: "SZL",
+	THB: "THB",
+	TJS: "TJS",
+	TMT: "TMT",
+	TND: "TND",
+	TOP: "TOP",
+	TRY: "TRY",
+	TTD: "TTD",
+	TWD: "TWD",
+	TZS: "TZS",
+	UAH: "UAH",
+	UGX: "UGX",
+	USD: "USD",
+	UYU: "UYU",
+	UZS: "UZS",
+	VES: "VES",
+	VND: "VND",
+	VUV: "VUV",
+	WST: "WST",
+	XAF: "XAF",
+	XCD: "XOF",
+	XOF: "XOF",
+	XPF: "XPF",
+	YER: "YER",
+	ZAR: "ZAR",
+	ZMW: "ZMW",
+	ZWL: "ZWL",
+}
+
+func (c *Currency) GetCurrencyISOCode() (string, error) {
+	isoCode, ok := currencyCode[*c]
+	if !ok {
+		return "", errors.New("invalid currency ISO code")
+	}
+
+	return isoCode, nil
+}
