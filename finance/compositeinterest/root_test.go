4fe1b4981a1fa456b9bb4aa14231151a13b3ea42
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-01-05 09:01:28 -0500

    feat: add new test for more complex operation about root

---
+++ finance/compositeinterest/root_test.go
@@ -0,0 +1,119 @@
+package compositeinterest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewPeriodSuccess(t *testing.T) {
+	period, err := NewPeriod(2, QuarterlyTwo)
+
+	tx := assert.New(t)
+
+	tx.NotNil(period, "period mustn't be nil")
+	tx.Nil(err)
+}
+
+func TestGetValuePeriod(t *testing.T) {
+	numberPeriod := 2.0
+	period, err := NewPeriod(numberPeriod, QuarterlyTwo)
+
+	tx := assert.New(t)
+
+	tx.NotNil(period, "period mustn't be nil")
+	tx.Nil(err)
+
+	value, _, err := period.getPeriod()
+
+	tx.Equal(numberPeriod, value)
+	tx.Nil(err)
+	tx.NoError(err)
+}
+
+func TestNewPeriodFailed(t *testing.T) {
+	period, err := NewPeriod(0, QuarterlyTwo)
+
+	tx := assert.New(t)
+
+	tx.Nil(period, "period must be nil")
+	tx.NotNil(err)
+}
+
+func TestNewPeriodWithDifferentsValues(t *testing.T) {
+	dataTest := []struct {
+		value                float64
+		compoundingFrequency CompoundingFrequency
+	}{
+		{
+			value:                2,
+			compoundingFrequency: Daily,
+		},
+		{
+			value:                5,
+			compoundingFrequency: Monthly,
+		},
+		{
+			value:                9,
+			compoundingFrequency: Bimonthly,
+		},
+		{
+			value:                1,
+			compoundingFrequency: QuarterlyOne,
+		},
+		{
+			value:                3,
+			compoundingFrequency: QuarterlyTwo,
+		},
+		{
+			value:                11,
+			compoundingFrequency: SemiAnnually,
+		},
+		{
+			value:                0.5,
+			compoundingFrequency: Annually,
+		},
+	}
+
+	for _, tt := range dataTest {
+		period, err := NewPeriod(tt.value, tt.compoundingFrequency)
+
+		tx := assert.New(t)
+
+		tx.NotNil(period, "period mustn't be nil")
+		tx.Nil(err)
+	}
+}
+
+func TestNewCompositeInterestZeroValue(t *testing.T) {
+	cr, err := New(0, 0, nil, nil)
+
+	tx := assert.New(t)
+
+	tx.Nil(cr)
+	tx.Error(err)
+}
+
+func TestGetEqualsRateInterestPeriods_differentTime(t *testing.T) {
+	period, err := NewPeriod(2, Bimonthly)
+
+	tx := assert.New(t)
+
+	tx.NotNil(period)
+	tx.NoError(err)
+
+	rate, err := NewRateInterest(0.05, Monthly, RateEffectyNominal)
+
+	tx.NotNil(rate)
+	tx.NoError(err)
+
+	compositeInterest, err := New(5_000, 0, rate, period)
+
+	tx.NotNil(compositeInterest)
+	tx.NoError(err)
+
+	future, err := compositeInterest.Future()
+
+	tx.NotNil(future)
+	tx.NoError(err)
+}
