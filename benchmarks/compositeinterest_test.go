8bd8f9bcabf86bcc3936da115ef39f5e2199948d
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-01-13 19:41:54 -0500

    feat: add test benchmark for package composite

---
+++ benchmarks/compositeinterest_test.go
@@ -0,0 +1,200 @@
+package benchmarks
+
+import (
+	"testing"
+
+	"github.com/yeferson59/gofinance/finance/compositeinterest"
+)
+
+func BenchmarkPeriod(b *testing.B) {
+	testcases := []float64{
+		6,
+		12,
+		18,
+		24,
+		36,
+		48,
+		0,
+	}
+
+	for _, value := range testcases {
+		b.StartTimer()
+		b.ReportAllocs()
+		for b.Loop() {
+			_, _ = compositeinterest.NewPeriod(value, compositeinterest.Monthly)
+		}
+	}
+}
+
+func BenchmarkNewRateInterest(b *testing.B) {
+	testcases := []struct {
+		value                float64
+		compoundingFrequency compositeinterest.CompoundingFrequency
+		typeRate             compositeinterest.TypeRate
+	}{
+		{
+			0.10,
+			compositeinterest.Annually,
+			compositeinterest.RateAnticipateEffectyAnnually,
+		},
+		{
+			0.24,
+			compositeinterest.Bimonthly,
+			compositeinterest.RateAnticipateEffectyNominal,
+		},
+		{
+			0.25,
+			compositeinterest.Annually,
+			compositeinterest.RateAnticipateEffectyPeriodic,
+		},
+		{
+			0.244,
+			compositeinterest.Daily,
+			compositeinterest.RateAnticipateEffectyNominal,
+		},
+	}
+
+	for _, testcase := range testcases {
+		b.StartTimer()
+		b.ReportAllocs()
+		for b.Loop() {
+			_, _ = compositeinterest.NewRateInterest(testcase.value, testcase.compoundingFrequency, testcase.typeRate)
+		}
+	}
+}
+
+func BenchmarkNewCompositeInterest(b *testing.B) {
+	testcases := []struct {
+		numberPeriod         float64
+		valueInterest        float64
+		compoundingFrequency compositeinterest.CompoundingFrequency
+		typeRate             compositeinterest.TypeRate
+		present              float64
+		future               float64
+	}{
+		{
+			2,
+			0.24,
+			compositeinterest.Monthly,
+			compositeinterest.RateEffectyNominal,
+			1_000,
+			0,
+		},
+		{
+			2.5,
+			0.24,
+			compositeinterest.Monthly,
+			compositeinterest.RateEffectyNominal,
+			0,
+			2_500,
+		},
+	}
+
+	for _, testcase := range testcases {
+		b.ReportAllocs()
+		b.StartTimer()
+		for b.Loop() {
+			period, _ := compositeinterest.NewPeriod(testcase.numberPeriod, testcase.compoundingFrequency)
+			rate, _ := compositeinterest.NewRateInterest(testcase.valueInterest, testcase.compoundingFrequency, testcase.typeRate)
+			_, _ = compositeinterest.New(testcase.present, testcase.future, rate, period)
+		}
+	}
+}
+
+func BenchmarkCompositeInterest(b *testing.B) {
+	testcases := []struct {
+		numberPeriod         float64
+		valueInterest        float64
+		compoundingFrequency compositeinterest.CompoundingFrequency
+		typeRate             compositeinterest.TypeRate
+		present              float64
+		future               float64
+	}{
+		{
+			2,
+			0.25,
+			compositeinterest.Monthly,
+			compositeinterest.RateEffectyNominal,
+			1_000,
+			0,
+		},
+		{
+			2.5,
+			0.24,
+			compositeinterest.Monthly,
+			compositeinterest.RateEffectyNominal,
+			0,
+			2_500,
+		},
+	}
+
+	b.Run("future", func(b *testing.B) {
+		for _, testcase := range testcases {
+			b.ReportAllocs()
+			b.StartTimer()
+			for b.Loop() {
+				period, _ := compositeinterest.NewPeriod(testcase.numberPeriod, testcase.compoundingFrequency)
+				rate, _ := compositeinterest.NewRateInterest(testcase.valueInterest, testcase.compoundingFrequency, testcase.typeRate)
+				ci, _ := compositeinterest.New(testcase.present, testcase.future, rate, period)
+
+				_, _ = ci.Future()
+			}
+		}
+	})
+
+	b.Run("present", func(b *testing.B) {
+		for _, testcase := range testcases {
+			b.ReportAllocs()
+			b.StartTimer()
+			for b.Loop() {
+				period, _ := compositeinterest.NewPeriod(testcase.numberPeriod, testcase.compoundingFrequency)
+				rate, _ := compositeinterest.NewRateInterest(testcase.valueInterest, testcase.compoundingFrequency, testcase.typeRate)
+				ci, _ := compositeinterest.New(testcase.present, testcase.future, rate, period)
+
+				_, _ = ci.Present()
+			}
+		}
+	})
+
+	b.Run("get equals rate interest periods", func(b *testing.B) {
+		for _, testcase := range testcases {
+			b.ReportAllocs()
+			b.StartTimer()
+			for b.Loop() {
+				period, _ := compositeinterest.NewPeriod(testcase.numberPeriod, testcase.compoundingFrequency)
+				rate, _ := compositeinterest.NewRateInterest(testcase.valueInterest, testcase.compoundingFrequency, testcase.typeRate)
+				ci, _ := compositeinterest.New(testcase.present, testcase.future, rate, period)
+
+				_, _, _ = ci.GetEqualsRateInterestPeriods()
+			}
+		}
+	})
+
+	b.Run("interest", func(b *testing.B) {
+		for _, testcase := range testcases {
+			b.ReportAllocs()
+			b.StartTimer()
+			for b.Loop() {
+				period, _ := compositeinterest.NewPeriod(testcase.numberPeriod, testcase.compoundingFrequency)
+				rate, _ := compositeinterest.NewRateInterest(testcase.valueInterest, testcase.compoundingFrequency, testcase.typeRate)
+				ci, _ := compositeinterest.New(testcase.present, testcase.future, rate, period)
+
+				_, _ = ci.Interest()
+			}
+		}
+	})
+
+	b.Run("periods", func(b *testing.B) {
+		for _, testcase := range testcases {
+			b.ReportAllocs()
+			b.StartTimer()
+			for b.Loop() {
+				period, _ := compositeinterest.NewPeriod(testcase.numberPeriod, testcase.compoundingFrequency)
+				rate, _ := compositeinterest.NewRateInterest(testcase.valueInterest, testcase.compoundingFrequency, testcase.typeRate)
+				ci, _ := compositeinterest.New(testcase.present, testcase.future, rate, period)
+
+				_, _ = ci.Periods()
+			}
+		}
+	})
+}
